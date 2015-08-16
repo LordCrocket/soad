0147945eff95801e62c52e5ffdf9d6c4d70942c4
rasmus <rasmus@larry>
2015-08-16 22:07:45 +0200

    Adds GameModule abstract class
    This adds the "GameModule" abstract class. This class will be subclassed
    by custom modules to add functionallity to the game.
    
    For now it only contains one method that takes the game state and
    updates it. This will probably change in the future to more fine grained
    methods.
    
    I am using a abstract class instead of a role to be able to have default
    method implementations. That way modules can ignore to implement some of
    the methods.

---
+++ GameModule.pm
@@ -0,0 +1,13 @@
+package GameModule;
+use Moose;
+use MooseX::ClassAttribute;
+use MooseX::ABC;
+use namespace::autoclean;
+
+sub update_game_state {} 
+
+
+
+__PACKAGE__->meta->make_immutable;
+
+1;
