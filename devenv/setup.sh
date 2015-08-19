43bb197ec06d20785977ce75acc833373a5a82b4
rasmus <rasmus@tesla>
2015-08-19 23:23:04 +0200

    Development environment run generate citizen tests

--- devenv/setup.sh
+++ devenv/setup.sh
@@ -8,7 +8,7 @@ cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mloca
 echo 'eval `perl -I ~/perl5/lib/perl5 -Mlocal::lib`' >> /home/vagrant/.profile
 cpanm -q MooseX::ABC String::Trim Readonly
 git clone https://github.com/LordCrocket/soad.git
-(cd soad/ ; perl t/GameState.t)
+(cd soad/ ; perl t/GameState.t ; perl t/GenerateCitizen.t)
 EOF
 
 
