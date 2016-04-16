e7a8e3291a2ec65e8fcee4f1c7767b5675ca5877
rasmus <rasmus@lovelace>
2016-04-16 14:08:10 +0200

    Adds missing dependencies to devenv

--- devenv/setup.sh
+++ devenv/setup.sh
@@ -6,7 +6,7 @@ curl -sL https://cpanmin.us | perl - App::cpanminus
 export PATH=/home/vagrant/perl5/bin/:$PATH
 cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
 echo 'eval `perl -I ~/perl5/lib/perl5 -Mlocal::lib`' >> /home/vagrant/.profile
-cpanm -q MooseX::ABC String::Trim Readonly List::Compare.pm
+cpanm -q MooseX::ABC String::Trim Readonly List::Compare.pm Clone Test::Exception Array/Utils.pm String/Numeric.pm
 git clone https://github.com/LordCrocket/soad.git
 (cd soad/ ; perl t/GameState.t ; perl t/GenerateCitizen.t)
 EOF
